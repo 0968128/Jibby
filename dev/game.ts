1265c6c1732865f6905485cff00a87feb8627a69
Lucas Hijman <46541848+0968128@users.noreply.github.com>
2020-08-02 20:42:38 +0200

    Update comments
    Comments voor verduidelijking zijn geüpdatet.

--- dev/game.ts
+++ dev/game.ts
@@ -2,20 +2,29 @@ class Game {
     // Fields
     private jibby:Jibby
 
+    // Methods
     constructor() {
+        // Maak een container aan om Jibby in te zetten
         let container = document.getElementById("container")!
         this.jibby = new Jibby(container)
+        
+        // Gameloop starten
         this.gameLoop()
     }
 
     private gameLoop(){
+        // Jibby's gedrag bepaalt wat er met hem gebeurt
         this.jibby.behavior.update()
+        
+        // Update de user interface (scores)
         this.updateUI()
+        
+        // Gameloop aan de gang houden
         requestAnimationFrame(() => this.gameLoop())
     }
 
     private updateUI():void{
-        // console.log(this.jibby.food, this.jibby.happiness, this.jibby.hygiene)
+        // Bereken Jibby's stats en geef in beeld hele getallen weer van het type string
         document.getElementsByTagName("food")[0].innerHTML = Math.round(this.jibby.food).toString()
         document.getElementsByTagName("happiness")[0].innerHTML = Math.round(this.jibby.happiness).toString()
         document.getElementsByTagName("hygiene")[0].innerHTML = Math.round(this.jibby.hygiene).toString()
